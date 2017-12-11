17dbf3ca9f72eb681ded0dceff1da70904ee58d2
Tobias Happ <tobias.happ@gmx.de>
2017-12-11 23:42:13 +0100

    Use hhvm 3.23 from nixpkgs

--- default.nix
+++ default.nix
@@ -5,17 +5,7 @@
 let
   composerEnv = import ./composer-env.nix {
     inherit (pkgs) stdenv writeTextFile fetchurl unzip;
-    php = pkgs.hhvm.overrideDerivation (old: rec {
-      name = "hhvm-${version}";
-      version = "3.23";
-
-      src = pkgs.fetchgit {
-        url    = "https://github.com/facebook/hhvm.git";
-        rev    = "3cc1735c3ab1690611ad8443094d2ccf951645ad";
-        sha256 = "1nic49j8nghx82lgvz0b95r78sqz46qaaqv4nx48p8yrj9ysnd7i";
-        fetchSubmodules = true;
-      };
-    });
+    php = import ./hhvm.nix;
   };
 in
 import ./php-packages.nix {
--- shell.nix
+++ shell.nix
@@ -4,17 +4,7 @@ stdenv.mkDerivation {
   name = "plc-web-hack";
 
   buildInputs = [
-    (hhvm.overrideDerivation (old: rec {
-      name = "hhvm-${version}";
-      version = "3.23";
-
-      src = fetchgit {
-        url    = "https://github.com/facebook/hhvm.git";
-        rev    = "3cc1735c3ab1690611ad8443094d2ccf951645ad";
-        sha256 = "1nic49j8nghx82lgvz0b95r78sqz46qaaqv4nx48p8yrj9ysnd7i";
-        fetchSubmodules = true;
-      };
-    }))
+    (import ./hhvm.nix)
     php
   ];
 }

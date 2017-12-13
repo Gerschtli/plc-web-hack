646984682428f5fc9e88f05bb19ea33095b36bbb
Tobias Happ <tobias.happ@gmx.de>
2017-12-13 14:29:41 +0100

    Updates hhvm expression

--- nixops/app.nix
+++ nixops/app.nix
@@ -23,17 +23,7 @@ rec {
       nixpkgs.overlays = [
         (self: super:
           {
-            hhvm = super.hhvm.overrideDerivation (old: rec {
-              name = "hhvm-${version}";
-              version = "3.23";
-
-              src = super.fetchgit {
-                url    = "https://github.com/facebook/hhvm.git";
-                rev    = "3cc1735c3ab1690611ad8443094d2ccf951645ad";
-                sha256 = "1nic49j8nghx82lgvz0b95r78sqz46qaaqv4nx48p8yrj9ysnd7i";
-                fetchSubmodules = true;
-              };
-            });
+            hhvm = import ../hhvm.nix;
           }
         )
       ];

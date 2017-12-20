830754679656365a97bc4fa3c4a9326122e3f5c1
Tobias Happ <tobias.happ@gmx.de>
2017-12-21 00:49:13 +0100

    Adds more util methods

--- src/Util/Globals.php
+++ src/Util/Globals.php
@@ -6,6 +6,16 @@ namespace PLC\Util;
 class Globals
 {
     /**
+     * Get global POST array as map.
+     *
+     * @return Map<string, mixed>  Typed POST array
+     */
+    public function getPost(): Map<string, mixed>
+    {
+        return new Map($_POST);
+    }
+
+    /**
      * Get global SERVER array as map.
      *
      * @return Map<string, mixed>  Typed SERVER array
@@ -13,5 +23,15 @@ class Globals
     public function getServer(): Map<string, mixed>
     {
         return new Map($_SERVER);
+    }
+
+    /**
+     * Get global SESSION array as map.
+     *
+     * @return Map<string, mixed>  Typed SESSION array
+     */
+    public function getSession(): Map<string, mixed>
+    {
+        return new Map($_SESSION);
     }
 }

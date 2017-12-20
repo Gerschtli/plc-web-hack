f88e663d7171f9646852727f1a3ef20b56339063
Tobias Happ <tobias.happ@gmx.de>
2017-12-20 14:45:44 +0100

    Updates init.sql

--- nixops/init.sql
+++ nixops/init.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE `blog`;
 
 CREATE TABLE `blog`.`user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `fullname` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `blog`.`article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL AUTO_INCREMENT,
   `body` text NOT NULL,
   `created_at` datetime NOT NULL,
   `teaser` text NOT NULL,
@@ -18,9 +18,9 @@ CREATE TABLE `blog`.`article` (
   `author_id` int(11) DEFAULT NULL,
   `body_html` text NOT NULL,
   `teaser_html` text NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`article_id`),
   KEY `FKgwrtdbqvt9ucntp82nd3yiuec` (`author_id`),
-  CONSTRAINT `FKgwrtdbqvt9ucntp82nd3yiuec` FOREIGN KEY (`author_id`) REFERENCES `blog`.`user` (`id`)
+  CONSTRAINT `FKgwrtdbqvt9ucntp82nd3yiuec` FOREIGN KEY (`author_id`) REFERENCES `blog`.`user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE USER 'plc'@'localhost' IDENTIFIED BY 'test';

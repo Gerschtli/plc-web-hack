8d95dbe4805dad5f1c326afcf93d4cfff6c989ae
Tobias Happ <tobias.happ@gmx.de>
2017-12-24 01:13:43 +0100

    Adds findByUsername and save in user service

--- src/Service/User.php
+++ src/Service/User.php
@@ -3,6 +3,7 @@
 namespace PLC\Service;
 
 use AsyncMysqlConnection;
+use AsyncMysqlQueryResult;
 use PLC\Model\User as UserModel;
 
 class User
@@ -17,9 +18,54 @@ class User
             $id
         );
 
+        return $this->_firstRowToModel($result);
+    }
+
+    public async function findByUsername(string $username): Awaitable<?UserModel>
+    {
+        $result = await $this->_connection->queryf(
+            'SELECT user_id, fullname, username, password FROM user WHERE username = %s',
+            $username
+        );
+
+        return $this->_firstRowToModel($result);
+    }
+
+    public async function save(UserModel $user): Awaitable<void>
+    {
+        if ($user->getId() === null) {
+            await $this->_insert($user);
+        } else {
+            await $this->_update($user);
+        }
+    }
+
+    private function _firstRowToModel(AsyncMysqlQueryResult $result): ?UserModel
+    {
         if ($result->numRows() == 0) {
             return null;
         }
         return UserModel::create($result->mapRowsTyped()->at(0));
+    }
+
+    private async function _insert(UserModel $user): Awaitable<void>
+    {
+        await $this->_connection->queryf(
+            'INSERT INTO user (fullname, username, password) VALUES (%s, %s, %s)',
+            $user->getFullname(),
+            $user->getUsername(),
+            $user->getPassword()
+        );
+    }
+
+    private async function _update(UserModel $user): Awaitable<void>
+    {
+        await $this->_connection->queryf(
+            'UPDATE user SET fullname = %s, username = %s, password = %s WHERE user_id = %d',
+            $user->getFullname(),
+            $user->getUsername(),
+            $user->getPassword(),
+            $user->getId()
+        );
     }
 }

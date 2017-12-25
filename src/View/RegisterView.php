16eb2c615ab076d77ddf70cff782c892f011cf42
Tobias Happ <tobias.happ@gmx.de>
2017-12-25 01:40:27 +0100

    Refactores form view

--- src/View/RegisterView.php
+++ src/View/RegisterView.php
@@ -20,54 +20,19 @@ class RegisterView extends View<Register> implements Viewable
             <plc:layout title="Registrieren">
                 <h1>Registrieren</h1>
                 <form action="/register" method="post">
-                    {$this->_errors($model->getErrors())}
-                    {$this->_success($model->isSuccess())}
-                    {$this->_formRow('username', 'Username', $user?->getUsername(), false)}
-                    {$this->_formRow('fullname', 'Voller Name', $user?->getFullname(), false)}
-                    {$this->_formRow('password', 'Passwort', null, true)}
-                    {$this->_formRow('password_repeat', 'Wiederhole Passwort', null, true)}
-                    <button type="submit" name="submit">Registrieren</button>
+                    {$this->_renderErrors($model->getErrors())}
+                    {$this->_renderSuccess(
+                        $model->isSuccess(),
+                        <x:frag>
+                            Registrierung erfolgreich. <a href="/login">Login</a>.
+                        </x:frag>
+                    )}
+                    {$this->_renderFormRow('username', 'Username', false, $user?->getUsername())}
+                    {$this->_renderFormRow('fullname', 'Voller Name', false, $user?->getFullname())}
+                    {$this->_renderFormRow('password', 'Passwort', true)}
+                    {$this->_renderFormRow('password_repeat', 'Wiederhole Passwort', true)}
+                    {$this->_renderFormSubmit('Registrieren')}
                 </form>
             </plc:layout>;
-    }
-
-    private function _errors(?Vector<string> $errors): :xhp
-    {
-        if ($errors === null || $errors->isEmpty()) {
-            return <x:frag />;
-        }
-
-        $result = <ul />;
-
-        foreach ($errors as $error) {
-            $result->appendChild(
-                <li>{$error}</li>
-            );
-        }
-
-        return <div class="error">{$result}</div>;
-    }
-
-    private function _formRow(string $name, string $label, ?Stringish $value, bool $isPassword): :xhp
-    {
-        $type = $isPassword ? 'password' : 'text';
-
-        return
-            <x:frag>
-                <label for={$name}>{$label}</label>
-                <input type={$type} id={$name} name={$name} value={$value} />
-            </x:frag>;
-    }
-
-    private function _success(bool $isSuccess): :xhp
-    {
-        if (!$isSuccess) {
-            return <x:frag />;
-        }
-
-        return
-            <p class="success">
-                Registrierung erfolgreich. <a href="/login">Login</a>.
-            </p>;
     }
 }
--- src/View/View.php
+++ src/View/View.php
@@ -42,4 +42,46 @@ abstract class View<T as Model>
      * @return :xhp           HTML page
      */
     protected abstract function _render(T $model): :xhp;
+
+    protected function _renderErrors(?Vector<string> $errors): :xhp
+    {
+        if ($errors === null || $errors->isEmpty()) {
+            return <x:frag />;
+        }
+
+        $result = <ul />;
+
+        foreach ($errors as $error) {
+            $result->appendChild(
+                <li>{$error}</li>
+            );
+        }
+
+        return <div class="error">{$result}</div>;
+    }
+
+    protected function _renderFormRow(string $name, string $label, bool $isPassword, ?Stringish $value = null): :xhp
+    {
+        $type = $isPassword ? 'password' : 'text';
+
+        return
+            <x:frag>
+                <label for={$name}>{$label}</label>
+                <input type={$type} id={$name} name={$name} value={$value} />
+            </x:frag>;
+    }
+
+    protected function _renderFormSubmit(string $value): :xhp
+    {
+        return <button type="submit" name="submit">{$value}</button>;
+    }
+
+    protected function _renderSuccess(bool $isSuccess, :xhp $message): :xhp
+    {
+        if (!$isSuccess) {
+            return <x:frag />;
+        }
+
+        return <p class="success">{$message}</p>;
+    }
 }

167987bde674a3a41fab4faf3478f3dac3b04222
Tobias Happ <tobias.happ@gmx.de>
2018-05-06 23:11:01 +0200

    Fixes composer in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,23 @@
 with import <nixpkgs> { };
 
+let
+  composer = phpPackages.composer.overrideAttrs (old: {
+    name = "hhvm-composer";
+
+    installPhase = ''
+      mkdir -p $out/bin
+      install -D $src $out/libexec/composer/composer.phar
+      makeWrapper ${hhvm}/bin/hhvm $out/bin/composer \
+        --add-flags "$out/libexec/composer/composer.phar"
+    '';
+  });
+in
+
 stdenv.mkDerivation {
   name = "plc-web-hack";
 
   buildInputs = [
-    phpPackages.composer
+    composer
     hhvm
   ];
 }
